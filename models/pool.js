214a4c031448a03ed5eb3640ffad2381f256d46c
HussainHaider <hussainhaider490@gmail.com>
2022-07-08 18:16:19 +0500

    remove console logs

--- models/pool.js
+++ models/pool.js
@@ -2,9 +2,7 @@ const Pool = require('pg-pool')
 const dotenv = require('dotenv');
 
 dotenv.config();
-console.log('process.env')
-console.log(process.env)
-console.log(process.env.DATABASE_URL)
+
 const databaseConfig = { connectionString: process.env.DATABASE_URL };
 const pool = new Pool(databaseConfig);
 
